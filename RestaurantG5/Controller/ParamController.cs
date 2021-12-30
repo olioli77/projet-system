9fc45193f9edd1ec2f04f33469372bf7154bc426
shamir-jae <78706774+shamir-jae@users.noreply.github.com>
2021-12-30 19:47:40 +0100

    Update ParamController.cs

--- RestaurantG5/Controller/ParamController.cs
+++ RestaurantG5/Controller/ParamController.cs
@@ -1,1 +1,21 @@
+using System.Configuration;
 
+namespace RestaurantG5.Controller
+{
+    public class ParamController
+    {
+        public static string GetValueOrDefault(string key, string defaultValue, bool numericalValue = false)
+        {
+            if (numericalValue == true)
+            {
+                string value = ConfigurationManager.AppSettings[key];
+                if (int.TryParse(value, out int result))
+                    return value;
+                else
+                    return defaultValue;
+            }
+            return ConfigurationManager.AppSettings[key] ?? defaultValue;
+
+        }
+    }
+}
